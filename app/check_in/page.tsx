e5a7dba0e60309dc7c8c02133b2bdc228446d7cc
yuuuuu <sopheak0891@gmail.com>
2025-09-12 10:54:25 +0700

    add new feature to mood_history

---
+++ app/check_in/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BackBtn from "../components/BackBtn";
+
+export default function page() {
+  return (
+    <div className="h-screen ">
+      <BackBtn />
+      <h1 className="text-3xl font-bold text-purple-500 text-center">
+        Coming Soon .......
+      </h1>
+    </div>
+  );
+}
--- app/components/BottomDock.tsx
+++ app/components/BottomDock.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
+import { LucidePackageCheck } from "lucide-react";
+
 import Link from "next/link";
 import { GoHome, GoHistory } from "react-icons/go";
 import { TiCogOutline } from "react-icons/ti";
@@ -10,6 +12,7 @@ export default function Docks() {
 
   const links = [
     { name: "Home", icon: <GoHome />, herf: "/" },
+    { name: "Check In", icon: <LucidePackageCheck />, herf: "/check_in" },
     { name: "History", icon: <GoHistory />, herf: "/mood_history" },
     { name: "Settings", icon: <TiCogOutline />, herf: "/sitting" },
   ];
@@ -21,7 +24,7 @@ export default function Docks() {
       {links.map((dock, index) => (
         <Link
           key={index}
-          className="flex font-bold items-center gap-1 rounded-xl border shadow-md p-3 bg-purple-500 text-white hover:bg-white hover:text-purple-500 transition-all"
+          className="flex font-bold  items-center gap-1 rounded-xl border shadow-md p-3 bg-purple-500 text-white hover:bg-white hover:text-purple-500 transition-all"
           href={dock.herf}
         >
           {dock.icon}
